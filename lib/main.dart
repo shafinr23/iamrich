7399eb69b13205978992e647a5567102fbe2e22e
shafinr23 <shafinr23@gmail.com>
2019-12-27 22:19:52 +0600

    add files

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//the main function of flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
